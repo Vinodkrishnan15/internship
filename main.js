854e690bd61fe4996131895b4dd8acb3c2f0cb77
vinod015 <vinodkrishnan012@gmail.com>
2024-02-24 10:32:42 +0530

    test 4

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 const imageContainer = document.querySelector('.image-gallery');
 
-// Assuming your image data is in a separate array or object
+
 const images = [
-  { image: "images1.jpg", caption: "Doctor 1" },
-  { image: "images2.jpg", caption: "Doctor 2" },
-  // ... add remaining image data
+  { image: "images1.jpg", caption: "Dr Victor Jhon" },
+  { image: "images2.jpg", caption: "Dr Avin Kumar" },
+  { image: "images3.jpg", caption: "Dr Renukha menon" },
+  { image: "images4.jpg", caption: "Dr Achuth Krishna" },
+  { image: "images5.jpg", caption: "Dr Bilal nasser" },
+  { image: "images6.jpg", caption: "Dr Sanoj Menon" },
+  { image: "images7.jpg", caption: "Dr Sarang K R" },
+  { image: "images8.jpg", caption: "Dr Ravi S" },
+  { image: "images9.jpg", caption: "Dr Nandhana P" },
+  { image: "images10.jpg", caption: "Dr Santhosh K" },
+  { image: "images11.jpg", caption: "Dr Kesav Kumar" },
+  { image: "images12.jpg", caption: "Dr Bhagaya devi" },
+  { image: "images13.jpg", caption: "Dr Jhon Frank" },
+  { image: "images14.jpg", caption: "Dr Radhika s" },
+  { image: "images15.jpg", caption: "Dr Vargese kurian" },
+  { image: "images16.jpg", caption: "Dr Sujatha m" },
+  { image: "images17.jpg", caption: "Dr Martin jacob" },
+  { image: "images18.jpg", caption: "Dr Kesav s " },
+  { image: "images19.jpg", caption: "Dr Avanthika k" },
+  { image: "images20.jpg", caption: "Dr Raseena t" },
+  { image: "images21.jpg", caption: "Dr Aayana kumar" },
+  { image: "images22.jpg", caption: "Dr shana k" },
+  { image: "images23.jpg", caption: "Dr Jhon jacob" },
+  { image: "images24.jpg", caption: "Dr Mammoty p" },
+
+
+
 ];
 
 images.forEach((image, index) => {
